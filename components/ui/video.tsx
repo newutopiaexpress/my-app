4f0b407dd332ca07239733ad38691ed4261f40f1
rick-sys <ricksanchezy2x@gmail.com>
2024-05-12 00:53:23 +0200

    wsy

--- components/ui/video.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-export function Video() {
-    return (
-        <video width="567" height="255" autoPlay loop muted playsInline preload="none">
-          <source src="/lucy.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-    )
-  }
